e15930bdea4e76ce94f99124bc44a20345cb8c29
Sergey Tolkachev <s15t96@yandex.ru>
2023-07-21 01:41:46 +0500

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,9 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	// Напиши код здесь
+    boolean expected = true;
+    
+    assertEquals("Человеку меньше 18", expected, isAdult);
+}
